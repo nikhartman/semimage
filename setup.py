0746e850f6c03a8fa3ba48a7c3b72abf2dc7327b
nikhartman <nik.hartman@gmail.com>
2015-01-17 14:11:34 -0500

    edit setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'semimage'
+      version = '1.0'
+      description = 'tools for SEM images'
+      author = 'Nik Hartman'
+      author_email = 'nik.hartman@gmail.com'
+      url = 'https://github.com/nikhartman/semimage'
+      packages = ['']
+      )
