df3d7fd3978d7aa20606d85881723c487f985723
nikhartman <nik.hartman@gmail.com>
2015-01-17 14:46:40 -0500

    attempt at using setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name = 'semimage'
-      version = '1.0'
-      description = 'tools for SEM images'
-      author = 'Nik Hartman'
-      author_email = 'nik.hartman@gmail.com'
-      url = 'https://github.com/nikhartman/semimage'
-      packages = ['']
+setup(name = 'semimage',
+      version = '1.0',
+      description = 'tools for SEM images',
+      author = 'Nik Hartman',
+      author_email = 'nik.hartman@gmail.com',
+      url = 'https://github.com/nikhartman/semimage',
+      packages = ['semimage'],
       )
